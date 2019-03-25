2eac1a5de88196d4a67de7289aeaff5d531cdb0b
Minxing Tang <minxing.tang@informatik.hu-berlin.de>
2019-03-25 10:24:40 +0100

    add error handling for memory/file

---
+++ src/runtime/ErrorHandling.h
@@ -0,0 +1,16 @@
+//
+//  ErrorHandling.h
+//
+//  Handling dynamic memory alloc errors and file write/read errors
+//
+//  Created by Minxing on 2019/3/21.
+//
+
+#ifndef ERROR_HANDLING_h
+#define ERROR_HANDLING_h
+
+report_error_malloc_space();
+report_error_read_file();
+report_error_write_file();
+
+#endif /* ERROR_HANDLING_h */
