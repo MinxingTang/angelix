a34a689d4c19420f6f7422b365e2d27121953a2e
Minxing <minxing_tang@yahoo.de>
2019-03-26 09:38:55 +0100

    add compatibility for C/C++ source code

--- src/runtime/ErrorHandling.h
+++ src/runtime/ErrorHandling.h
@@ -9,9 +9,15 @@
 #ifndef ERROR_HANDLING_h
 #define ERROR_HANDLING_h
 
-void report_error_malloc_space();
-void report_error_read_file();
-void report_error_write_file();
-void report_error_make_dir();
+#ifdef __cplusplus
+extern "C"{
+#endif
+    void report_error_malloc_space();
+    void report_error_read_file();
+    void report_error_write_file();
+    void report_error_make_dir();
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* ERROR_HANDLING_h */
